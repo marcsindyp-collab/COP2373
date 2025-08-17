10b50112a2771badbf85e43ac0be639b93515300
Marcsindy <Marcsindyp@gmail.com>
2025-08-17 11:11:30 -0400

    Initial commit

---
+++ COP2737.py
@@ -0,0 +1,23 @@
+import pygame  # todo import other modules
+
+pygame.init()
+
+test = 7
+print(test)
+print(test)
+print(test)
+print(test)
+
+def main():  # todo finish this function
+    hello = 0
+    print(hello)
+    print(hello)
+    print(hello)
+    print(hello)
+    print(hello)
+run = True
+
+try:
+    run
+except:
+    pass
